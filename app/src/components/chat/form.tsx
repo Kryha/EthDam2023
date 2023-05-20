63ea73ed4f46bb0823a6e6a6cdce3196ce69271a
Frank Benjamin Arias Padilla <benjamin@kryha.io>
2023-05-20 23:10:33 +0200

    added FE changes

--- app/src/components/chat/form.tsx
+++ app/src/components/chat/form.tsx
@@ -49,7 +49,7 @@ export const Form = () => {
 			}}
 		>
 			<Stack direction="row" justifyContent="space-between" sx={{ width: 1 }}>
-				<TextField {...register("message")} name="message" id="standard-basic" label="Write something" variant="standard" />
+				<TextField {...register("message")} color="secondary" name="message" id="standard-basic" label="Write something" variant="standard" />
 				<Button variant="contained" color="secondary" type="submit">
 					Send
 				</Button>
