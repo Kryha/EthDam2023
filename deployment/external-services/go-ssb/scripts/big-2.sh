6d9281a5272554f3a0716f85223fe5209a02d29b
Filip Harald <filip@kryha.io>
2023-05-20 14:21:28 +0200

    changed sbot set-up

--- deployment/external-services/go-ssb/scripts/big-2.sh
+++ deployment/external-services/go-ssb/scripts/big-2.sh
@@ -1,4 +1,4 @@
-sbotcli connect "net:ethdam2023-go-ssb-1-1:8008~shs:xkgJow7CjglAfYLVSSmDkkc8rEeM3eJ0mPi8jTm7hZs="
 sbotcli publish contact --following '@xkgJow7CjglAfYLVSSmDkkc8rEeM3eJ0mPi8jTm7hZs=.ed25519'
+sbotcli connect "net:ethdam2023-go-ssb-1-1:8008~shs:xkgJow7CjglAfYLVSSmDkkc8rEeM3eJ0mPi8jTm7hZs="
 sbotcli publish about --name 'go-1' '@xkgJow7CjglAfYLVSSmDkkc8rEeM3eJ0mPi8jTm7hZs=.ed25519'
 sbotcli publish post "hello I am nbr 2"
