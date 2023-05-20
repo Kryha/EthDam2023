777eb8b8fc41ccf23d0736290dd3c0897298e6a2
Axel Verheul <axel@kryha.io>
2023-05-21 00:15:17 +0200

    sc fix

--- app/src/services/contract-call.ts
+++ app/src/services/contract-call.ts
@@ -101,7 +101,7 @@ export const useContractGetMessageById = () => {
 
   const getVoteCountByMessageId = async (messageId: string) => {
     const tr = await callGetMessageById(messageId);
-    const votes = Number(tr[1]);
+    const votes = tr || 0;
     return votes;
   };
 
