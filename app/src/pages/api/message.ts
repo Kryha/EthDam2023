54a66ffc9df6e76858fd140ba17d6f3416d4b390
Axel Verheul <axel@kryha.io>
2023-05-20 16:40:39 +0200

    removed console.log

--- app/src/pages/api/message.ts
+++ app/src/pages/api/message.ts
@@ -62,7 +62,6 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log(req.method);
   switch (req.method) {
     case "GET":
       return GET(req, res);
