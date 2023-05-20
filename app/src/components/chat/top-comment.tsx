f46853d9e585366c52f58d1856e0cfc0dae6b2a9
Frank Benjamin Arias Padilla <benjamin@kryha.io>
2023-05-20 22:57:09 +0200

    fixes fro FE

--- app/src/components/chat/top-comment.tsx
+++ app/src/components/chat/top-comment.tsx
@@ -37,53 +37,43 @@ export const TopComment = (props: Props) => {
 				borderRadius: 2,
 				width: 1,
 				overflow: "hidden",
+				...rest.sx,
 			}}
 		>
+			<Stack direction="row" spacing={2} sx={{ p: 1 }}>
+				<Avatar />
+				<Stack>
+					<Typography variant="body1" fontWeight="bold" color="secondary">
+						{username}
+					</Typography>
+					<Typography variant="body1" color="secondary">
+						{message}
+					</Typography>
+				</Stack>
+			</Stack>
 			<Stack
-				key={key}
-				component={Paper}
+				direction="row"
+				justifyContent="space-between"
+				spacing={1}
+				px={1}
 				sx={{
-					borderRadius: 2,
-					width: 1,
-					overflow: "hidden",
-					...rest.sx,
+					// p: 0.2,
+					bgcolor: "primary.main",
+					background: "linear-gradient(70deg, #67CFEB, #A9EA50, #CA38EB,#67CFEB, #A9EA50)",
+					backgroundSize: "200% 100%",
+					animation: "gradient 5s ease alternate-reverse infinite",
 				}}
 			>
-				<Stack direction="row" spacing={2} sx={{ p: 1 }}>
-					<Avatar />
-					<Stack>
-						<Typography variant="body1" fontWeight="bold" color="secondary">
-							{username}
-						</Typography>
-						<Typography variant="body1" color="secondary">
-							{message}
-						</Typography>
-					</Stack>
-				</Stack>
-				<Stack
-					direction="row"
-					justifyContent="space-between"
-					spacing={1}
-					px={1}
-					sx={{
-						// p: 0.2,
-						bgcolor: "primary.main",
-						background: "linear-gradient(70deg, #67CFEB, #A9EA50, #CA38EB,#67CFEB, #A9EA50)",
-						backgroundSize: "200% 100%",
-						animation: "gradient 5s ease alternate-reverse infinite",
-					}}
-				>
-					<Typography variant="h6" color="primary">
-						{count}
-					</Typography>
-					<Stack direction="row" spacing={2}>
-						<IconButton size="small" sx={{ bgcolor: "primary.main" }}>
-							<North fontSize="small" color="secondary" />
-						</IconButton>
-						<IconButton size="small" sx={{ bgcolor: "primary.main" }}>
-							<South fontSize="small" color="secondary" />
-						</IconButton>
-					</Stack>
+				<Typography variant="h6" color="primary">
+					{count}
+				</Typography>
+				<Stack direction="row" spacing={2}>
+					<IconButton size="small" sx={{ bgcolor: "primary.main" }}>
+						<North fontSize="small" color="secondary" />
+					</IconButton>
+					<IconButton size="small" sx={{ bgcolor: "primary.main" }}>
+						<South fontSize="small" color="secondary" />
+					</IconButton>
 				</Stack>
 			</Stack>
 		</Stack>
