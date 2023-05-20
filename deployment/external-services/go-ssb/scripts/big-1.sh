9605a7d3857496f1011663acfc22841a5c177a75
Filip Harald <filip@kryha.io>
2023-05-20 12:55:41 +0200

    some go-ssb-stuff

---
+++ deployment/external-services/go-ssb/scripts/big-1.sh
@@ -0,0 +1,3 @@
+sbotcli publish contact --following '@4VZr4YqV+7ZE+GkOFBw5nJ8vx7Zlfi9FHefqSdF648Q=.ed25519'
+sbotcli publish about --name 'go-2' '@4VZr4YqV+7ZE+GkOFBw5nJ8vx7Zlfi9FHefqSdF648Q=.ed25519'
+sbotcli publish post "wazzzaaa"
---
+++ deployment/external-services/go-ssb/scripts/big-2.sh
@@ -0,0 +1,3 @@
+sbotcli publish contact --following '@xkgJow7CjglAfYLVSSmDkkc8rEeM3eJ0mPi8jTm7hZs=.ed25519'
+sbotcli publish about --name 'go-1' '@xkgJow7CjglAfYLVSSmDkkc8rEeM3eJ0mPi8jTm7hZs=.ed25519'
+sbotcli publish post "wazzzaaa"
