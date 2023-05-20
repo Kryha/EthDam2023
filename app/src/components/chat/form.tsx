a1ca3360f507754bafdb86bf2ffa114f7d52fb81
Axel Verheul <axel@kryha.io>
2023-05-20 23:24:01 +0200

    updated js code

--- app/src/components/chat/form.tsx
+++ app/src/components/chat/form.tsx
@@ -1,5 +1,5 @@
 import { Button, Slider, Stack, TextField, Typography } from "@mui/material";
-import { useContractConnectPost, useContractConnectUpvote } from "@services/contract-call";
+import { useContractConnectUpvote } from "@services/contract-call";
 import axios from "axios";
 import { useMemo, useState } from "react";
 import { useForm } from "react-hook-form";
@@ -7,59 +7,81 @@ import { useMutation } from "react-query";
 
 type MessageContent = { amount: number; message: string; username: string };
 
-export const postMessage = async ({ message, special }: { message: string; special: boolean }): Promise<string> => {
-	const response = await axios.post("api/message", { message, special });
-	return response.data.messageId;
+export const postMessage = async ({
+  message,
+  special,
+}: {
+  message: string;
+  special: boolean;
+}): Promise<string> => {
+  const response = await axios.post("api/message", { message, special });
+  return response.data.messageId;
 };
 
 export const Form = () => {
-	const { data: messageId, mutate: post } = useMutation(postMessage);
-	const formMethods = useForm<MessageContent>({ mode: "onChange" });
-	const { register, handleSubmit, watch } = formMethods;
-	const [slider, setSlider] = useState<number | number[]>(0);
+  const { data: messageId, mutate: post } = useMutation(postMessage);
+  const formMethods = useForm<MessageContent>({ mode: "onChange" });
+  const { register, handleSubmit, watch } = formMethods;
+  const [slider, setSlider] = useState<number | number[]>(0);
 
-	const { callPostMessage } = useContractConnectPost();
-	const { callUpvote } = useContractConnectUpvote();
+  const { callUpvote } = useContractConnectUpvote();
 
-	const onSubmit = async ({ message, amount }: MessageContent) => {
-		post({ message: message, special: amount > 0 });
-	};
+  const onSubmit = async ({ message, amount }: MessageContent) => {
+    post({ message: message, special: amount > 0 });
+  };
 
-	useMemo(async () => {
-		if (messageId) {
-			console.log(messageId);
-			await callPostMessage("", messageId);
-			await callUpvote(messageId);
-		}
-	}, [messageId]);
+  useMemo(async () => {
+    if (messageId && !Array.isArray(slider) && slider > 0) {
+      await callUpvote(messageId, slider);
+    }
+  }, [messageId, slider]);
 
-	return (
-		<Stack
-			component="form"
-			onSubmit={handleSubmit(onSubmit)}
-			spacing={3}
-			sx={{
-				flexGrow: 1,
-				maxHeight: 140,
-				justifyContent: "center",
-				alignItems: "center",
-				px: 2,
-				py: 1,
-				borderTop: 1,
-			}}
-		>
-			<Stack direction="row" justifyContent="space-between" sx={{ width: 1 }}>
-				<TextField {...register("message")} color="secondary" name="message" id="standard-basic" label="Write something" variant="standard" />
-				<Button variant="contained" color="secondary" type="submit">
-					Send
-				</Button>
-			</Stack>
-			<Stack direction="row" sx={{ width: 1 }} spacing={4}>
-				<Slider {...register("amount")} value={slider} onChange={(_, value) => setSlider(value)} color="secondary" name="amount" defaultValue={1} step={1} marks min={0} valueLabelDisplay="auto" max={10} />
-				<Typography variant="h5" color="secondary">
-					{slider}
-				</Typography>
-			</Stack>
-		</Stack>
-	);
+  return (
+    <Stack
+      component="form"
+      onSubmit={handleSubmit(onSubmit)}
+      spacing={3}
+      sx={{
+        flexGrow: 1,
+        maxHeight: 140,
+        justifyContent: "center",
+        alignItems: "center",
+        px: 2,
+        py: 1,
+        borderTop: 1,
+      }}
+    >
+      <Stack direction="row" justifyContent="space-between" sx={{ width: 1 }}>
+        <TextField
+          {...register("message")}
+          color="secondary"
+          name="message"
+          id="standard-basic"
+          label="Write something"
+          variant="standard"
+        />
+        <Button variant="contained" color="secondary" type="submit">
+          Send
+        </Button>
+      </Stack>
+      <Stack direction="row" sx={{ width: 1 }} spacing={4}>
+        <Slider
+          {...register("amount")}
+          value={slider}
+          onChange={(_, value) => setSlider(value)}
+          color="secondary"
+          name="amount"
+          defaultValue={1}
+          step={1}
+          marks
+          min={0}
+          valueLabelDisplay="auto"
+          max={10}
+        />
+        <Typography variant="h5" color="secondary">
+          {slider}
+        </Typography>
+      </Stack>
+    </Stack>
+  );
 };
--- app/src/features/wallet/wallet.tsx
+++ app/src/features/wallet/wallet.tsx
@@ -1,29 +1,27 @@
-import { useWeb3Connect, useContractConnectPost, useContractConnectUpvote, useContractConnectDownvote, useContractGetMessageById } from "@/services";
+import {
+  useWeb3Connect,
+  useContractConnectUpvote,
+  useContractConnectDownvote,
+  useContractGetMessageById,
+} from "@/services";
 import { useStore } from "@/store";
 import { Button, Typography } from "@mui/material";
 
 export const Wallet = () => {
   const client = useStore((state) => state.client);
   const { connectWeb3 } = useWeb3Connect();
-  const { callPostMessage } = useContractConnectPost();
   const { callUpvote } = useContractConnectUpvote();
   const { callDownvote } = useContractConnectDownvote();
   const { callGetMessageById } = useContractGetMessageById();
-
-
-
 
   if (client && client.isConnected)
     return (
       <>
         <Button variant="contained"> Disconnect</Button>
-        <Button variant="contained" onClick={() => callPostMessage("a", "b")}>
-          Call contract Post
-        </Button>
-        <Button variant="contained" onClick={() => callUpvote("b")}>
+        <Button variant="contained" onClick={() => callUpvote("b", 1)}>
           Call contract upvote
         </Button>
-        <Button variant="contained" onClick={() => callDownvote("b")}>
+        <Button variant="contained" onClick={() => callDownvote("b", 1)}>
           Call contract downvote
         </Button>
         <Button variant="contained" onClick={() => callGetMessageById("b")}>
