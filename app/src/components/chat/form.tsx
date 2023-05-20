63cf3e2845b277b480d0d946c251fc9ca249f773
Frank Benjamin Arias Padilla <benjamin@kryha.io>
2023-05-20 14:32:53 +0200

    added post components

--- app/src/components/chat/form.tsx
+++ app/src/components/chat/form.tsx
@@ -1,32 +1,30 @@
-import { Button, FormControl, Slider, Stack, TextField } from "@mui/material";
-import React, { FormEvent, FormEventHandler } from "react";
-import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { Button, Slider, Stack, TextField } from "@mui/material";
+import { useForm } from "react-hook-form";
+
+type MessageContent = { message: string; special?: boolean; username: string };
 
 export const Form = () => {
-	const {
-		register,
-		handleSubmit,
-		watch,
-		formState: { errors },
-	} = useForm();
+	const formMethods = useForm<MessageContent>({ mode: "onChange" });
+	const { register, handleSubmit } = formMethods;
 
-	const onSubmit = (event: SubmitHandler<FieldValues>): void => {
-		// event.preventDefault();
-		console.log("submit", event, {});
+	const onSubmit = ({ message }: MessageContent) => {
+		const send = {
+			message: message,
+			special: false,
+			username: "joe",
+		};
+		console.log({ send });
 	};
 
-	console.log(watch("example")); // watch input value by passing the name of it
-
 	return (
-		<></>
-		// <Stack component="form" onSubmit={handleSubmit(onSubmit)} spacing={3} sx={{ border: 1, flexGrow: 1, maxHeight: 140, justifyContent: "center", alignItems: "center" }}>
-		// 	<Stack direction="row">
-		// 		<TextField name="message" id="standard-basic" label="Standard" variant="standard" />
-		// 		<Button variant="contained" type="submit">
-		// 			Send
-		// 		</Button>
-		// 	</Stack>
-		// 	<Slider name="slider" defaultValue={1} step={1} marks min={0} valueLabelDisplay="auto" max={10} />
-		// </Stack>
+		<Stack component="form" onSubmit={handleSubmit(onSubmit)} spacing={3} sx={{ border: 1, flexGrow: 1, maxHeight: 140, justifyContent: "center", alignItems: "center" }}>
+			<Stack direction="row">
+				<TextField {...register("message")} name="message" id="standard-basic" label="Standard" variant="standard" />
+				<Button variant="contained" type="submit">
+					Send
+				</Button>
+			</Stack>
+			{/* <Slider {...register("amount")} name="amount" defaultValue={1} step={1} marks min={0} valueLabelDisplay="auto" max={10} /> */}
+		</Stack>
 	);
 };
