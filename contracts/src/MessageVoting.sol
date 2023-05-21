3fc58aedb68df3d882ef5efdb8bba2ba40c43a18
ivan <ivan@kryha.io>
2023-05-21 09:28:59 +0200

    smart contract uses string to int

--- contracts/src/MessageVoting.sol
+++ contracts/src/MessageVoting.sol
@@ -1,17 +1,19 @@
 pragma solidity ^0.8.9;
 
 contract MessageVoting {
-    mapping(bytes32 => int) public voteCount;
+    mapping(string => int) public voteCount;
 
-    function upvote(bytes32 _messageId, int _voteCount) public {
+    function upvote(string memory _messageId, int _voteCount) public {
         voteCount[_messageId] += _voteCount;
     }
 
-    function downvote(bytes32 _messageId, int _voteCount) public {
+    function downvote(string memory _messageId, int _voteCount) public {
         voteCount[_messageId] -= _voteCount;
     }
 
-    function getMessageById(bytes32 _messageId) public view returns (int) {
+    function getMessageById(
+        string memory _messageId
+    ) public view returns (int) {
         return voteCount[_messageId];
     }
 }
